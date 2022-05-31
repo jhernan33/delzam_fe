499e6616dca4b31ad1437f2c4de3a7e976ec1589
Hernan <jhernan33@gmail.com>
2022-05-31 08:24:19 -0400

    proyecto de vue

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          :src="'/static/logo.png'" 
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          :src="getImgUrl+'logo.png'"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="#"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Clientes</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+    getImgUrl(pet) {
+      var images = require.context('/src/assets/', false, /\.png$/)
+      return images('./' + pet + ".png")
+    }
+  }),
+};
+
+</script>
