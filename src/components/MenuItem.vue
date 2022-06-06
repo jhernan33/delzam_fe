ef64dc597f26f1ae1001b0b702029416f87e89ae
Hernan <jhernan33@gmail.com>
2022-06-06 14:21:15 -0400

    prueba de link en el menu

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,168 +1,64 @@
-<!-- <template>
-  <v-card
-    height="400"
-    width="256"
-    class="mx-auto"
-  >
-    <v-navigation-drawer permanent>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            WebSite DelZam
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Mayorista
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
-</template>
-
+<div id="app">
+    <v-app>
+        <v-navigation-drawer app dark class="primary" v-model="drawer">
+            <v-list>
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="title">
+                            <span class="font-weight-bold">Al-Mehmood</span>
+                        </v-list-item-title>
+                        <v-list-item-subtitle>Transport Company</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-divider></v-divider>
+                <v-list-group no-action v-for='navLink in navLinks' :key="navLink.text">
+                    <v-list-item slot='activator' :to="navLink.route">
+                        <v-list-item-icon>
+                            <v-icon>{{ navLink.icon }}</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>{{ navLink.text }}</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item v-for='sub in navLink.subLinks' :key="sub.text">
+                        <v-list-item-title :to="sub.route">{{ sub.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list-group>
+            </v-list>
+        </v-navigation-drawer>
+    </v-app>
+</div>
 <script>
-  export default {
-    // name: 'MenuItem',
-    data () {
-      return {
-        items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Archivo', icon: 'mdi-image' },
-          { title: 'Proceso', icon: 'mdi-help-box' },
-        ],
-        right: null,
-      }
-    },
-  }
-</script> -->
-
-<template>
-  <v-card
-    class="mx-auto"
-    width="300"
-  >
-    <v-list>
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon>mdi-home</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-title>Principal</v-list-item-title>
-      </v-list-item>
-
-      <v-list-group
-        :value="true"
-        prepend-icon="mdi-account-circle"
-      >
-
-      <template v-slot:activator>
-        <v-list-item-content>
-          <v-list-item-title>Archivo</v-list-item-title>
-        </v-list-item-content>
-      </template>
-
-      <v-list-item
-        v-for="([title, icon], i) in files"
-        :key="i"
-        link
-      >
-      <v-list-item-icon>
-          <v-icon v-text="icon"></v-icon>
-        </v-list-item-icon>
-      
-      <v-list-item-title v-text="title"></v-list-item-title>
-      
-      </v-list-item>
-        
-        
-        <!-- <v-list-item>Familia</v-list-item>
-        <v-list-item>SubFamilia</v-list-item>
-        <v-list-item>Productos</v-list-item> -->
-        <!-- <v-list-group
-          :value="true"
-          no-action
-          sub-group
-        >
-          <template v-slot:activator>
-            <v-list-item-content>
-              <v-list-item-title>Familia</v-list-item-title>
-            </v-list-item-content>
-          </template>
-
-          <v-list-item
-            v-for="([title, icon], i) in admins"
-            :key="i"
-            link
-          >
-            <v-list-item-title v-text="title"></v-list-item-title>
-
-            <v-list-item-icon>
-              <v-icon v-text="icon"></v-icon>
-            </v-list-item-icon>
-          </v-list-item>
-        </v-list-group> -->
-
-        <!-- <v-list-group
-          no-action
-          sub-group
-        >
-          <template v-slot:activator>
-            <v-list-item-content>
-              <v-list-item-title>Actions</v-list-item-title>
-            </v-list-item-content>
-          </template>
-
-          <v-list-item
-            v-for="([title, icon], i) in cruds"
-            :key="i"
-            link
-          >
-            <v-list-item-title v-text="title"></v-list-item-title>
-
-            <v-list-item-icon>
-              <v-icon v-text="icon"></v-icon>
-            </v-list-item-icon>
-          </v-list-item>
-        </v-list-group> -->
-      </v-list-group>
-    </v-list>
-  </v-card>
-</template>
-
-<script>
-  export default {
-    data: () => ({
-      files: [
-        ['Familia',     'mdi-human-male-male','/family'],
-        ['SubFamilia',  'mdi-human-male-female-child','/subfamily'],
-        ['Producto',    'mdi-package-variant','/product']
-      ],
-      cruds: [
-        ['Create', 'mdi-plus-outline'],
-        ['Read', 'mdi-file-outline'],
-        ['Update', 'mdi-update'],
-        ['Delete', 'mdi-delete'],
-      ],
-    }),
-  }
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data() {
+        return {
+            drawer: true,
+    
+            navLinks: [
+                { icon: "dashboard", text: "Dashboard", route: "/" },
+                { icon: "star", text: "Ratings", route: "/ratings" },
+                {
+                    icon: "people",
+                    text: "Drivers",
+                    route: "/drivers",
+                    subLinks: [
+                        { text: "Add Driver", route: "/drivers/add" },
+                        { text: "Add Driver", route: "/drivers/add" }
+                    ]
+                },
+    
+                {
+                    icon: "person_outline",
+                    text: "Manager",
+                    route: "/managers",
+                    subLinks: [
+                        { text: "Add Manager", route: "/managers/add" },
+                        { text: "Managers", route: "/managers" }
+                    ]
+                },
+                { icon: "person", text: "Manage Users", route: "/users" }
+            ]
+        }
+    }
+})
 </script>
