6bea368cd59bebee387eb4367ffff6bcf5ba9ec3
Hernan <jhernan33@gmail.com>
2022-06-07 05:25:51 -0400

    actualizacion del modelo familia

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = defineConfig({
     'vuetify'
   ],
   devServer: {
-    host: '159.223.168.118',
+    //host: '159.223.168.118',
+    host: '192.168.0.101',
     //host: '172.16.0.7',
     port: 7500,
   },
